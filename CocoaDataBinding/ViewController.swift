8528ca3877a57381a96eb4aa974b384e98c22527
leiguang <575403441@qq.com>
2018-12-25 11:04:59 +0800

    Initial Commit

---
+++ CocoaDataBinding/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CocoaDataBinding
+//
+//  Created by 雷广 on 2018/12/25.
+//  Copyright © 2018 leiguang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
